b0cc19396f5427e2ff9f3d0fd31ae769b4f5eb40
jaredden <james.ifit@gmail.com>
2023-09-24 13:27:27 -0400

    added CRUD routes in routes/recipes.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 //localhost:4000 test
 router.get("/", function (req, res, next) {
