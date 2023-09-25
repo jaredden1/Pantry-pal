c96b0357909bdb10d6748a0b84332061e557da7d
jaredden <james.ifit@gmail.com>
2023-09-24 20:00:58 -0400

    added API and updated models/recipe.js schema

--- server/models/recipe.js
+++ server/models/recipe.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
-const recipeSchema = new Schema({
-
-});
+const recipeSchema = new Schema(
+  {
+    ingredients: { type: String },
+    number: { type: Number },
+    limitLicense: { type: Boolean },
+    ranking: { type: Number },
+    ignorePantry: { type: Boolean },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Recipe", recipeSchema);
