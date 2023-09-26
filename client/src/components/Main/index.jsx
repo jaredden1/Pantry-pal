682551ab6500c577fd609422654dea1aae91cb1b
jaredden <james.ifit@gmail.com>
2023-09-26 09:29:36 -0400

    added about/index and route in main

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import SearchBar from "../../pages/Search/SearchBar";
+import About from "../../pages/About";
 
 export default function () {
   return (
     <main className="main">
       <Routes>
         <Route path="/" element={<SearchBar />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </main>
   );
---
+++ client/src/pages/About/index.jsx
@@ -0,0 +1,7 @@
+
+export default function About() {
+    return (
+      <h1>My Recipes page</h1>
+
+    )
+}
