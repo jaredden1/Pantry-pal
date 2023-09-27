dc3cccf3a55cb6548c68ff02ea1cad9dcc737966
jaredden <james.ifit@gmail.com>
2023-09-26 20:08:41 -0400

    added handlers and succesfully connected client side to server side

--- client/src/pages/Search/SearchBar.jsx
+++ client/src/pages/Search/SearchBar.jsx
@@ -1,14 +1,40 @@
+import { useState } from "react";
+import { getRecipes } from "../../utilities/SearchAPI/searchService";
 import "./SearchBar.css";
 
-export default function SearchBar() {
-  async function handleSubmit() {
-    alert("test");
+const initState = {
+  ingredients: "",
+};
+
+export default function SearchBar({ setResults }) {
+  const [newSearch, setNewSearch] = useState(initState);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const results = await getRecipes(newSearch);
+    console.log(results)
+    if (results.id) {
+      setResults(results);
+    }
+    setNewSearch(initState);
   }
-      
+  function handleChange(e) {
+    const updatedQuery = { ...newSearch, [e.target.name]: e.target.value };
+    setNewSearch(updatedQuery);
+  }
+  
   return (
     <div className="searchbar">
       <form onSubmit={handleSubmit}>
-        <input className="search-input" type="text" placeholder="Enter Your Ingredients" required />
+      <input
+          type="text"
+          placeholder="Enter ingredients separated by a comma."
+          value={newSearch.ingredients}
+          name="ingredients"
+          onChange={handleChange}
+          required
+          className="search-input"
+        />
         <button type="submit" className="search-submit">
           Search
         </button>
