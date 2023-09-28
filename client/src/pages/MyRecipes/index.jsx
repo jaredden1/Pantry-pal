dcd3838948888038e0b0dfb3ec8533958a15a8e2
jaredden <james.ifit@gmail.com>
2023-09-28 19:07:48 -0400

    update

--- client/src/pages/MyRecipes/index.jsx
+++ client/src/pages/MyRecipes/index.jsx
@@ -1,74 +1,84 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { deleteRecipe } from '../../utilities/Recipe/recipesService';
+import { deleteRecipe } from "../../utilities/Recipe/recipesService";
 
 export default function MyRecipes() {
-    const [recipes, setRecipes] = useState([]);  
-    const [loading, setLoading] = useState(true); 
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        fetch('http://localhost:4000/recipes')
-            .then(response => response.json())
-            .then(data => {
-                setRecipes(data);
-                setLoading(false);
-            })
-            .catch(error => {
-                console.error("Error fetching recipes:", error);
-                setLoading(false);
-            });
-    }, []); 
+  useEffect(() => {
+    fetch("http://localhost:4000/recipes")
+      .then((response) => response.json())
+      .then((data) => {
+        setRecipes(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching recipes:", error);
+        setLoading(false);
+      });
+  }, []);
 
-    async function handleDelete(id) {
-        try {
-          const deleteResponse = await deleteRecipe(id).then(navigate(0));
-        } catch (error) {
-          console.log(error);
-        }
-      }
-
-    function stripHtml(html) {
-        if (!html) return "";
-        return html.replace(/<\/?[^>]+(>|$)/g, "");
+  async function handleDelete(id) {
+    try {
+      const deleteResponse = await deleteRecipe(id).then(navigate(0));
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    if (loading) {
-        return <p>Loading...</p>;
-    }
+  function stripHtml(html) {
+    if (!html) return "";
+    return html.replace(/<\/?[^>]+(>|$)/g, "");
+  }
 
-    return (
-        <div>
-            {recipes.map(recipe => (
-                <div key={recipe.id} className="searchResult">
-                    <h2>{recipe.title}</h2>
-                    <img src={recipe.image} alt={recipe.title}/>
-                    <p>{stripHtml(recipe.summary)}</p>
-                    <br />
-                    <p>Ingredients:{recipe.ingredients.map(ingredient=>(
-                       <div>
-                        <img
-                        src={`https://spoonacular.com/cdn/ingredients_100x100/${ingredient[1]}`}
-                        alt={ingredient[0]}
-                      />
-                      <p>{ingredient[0]}</p>
-                      </div> 
-                    ))}</p>
-                    <br />
-                    <p>Instructions:{recipe.instructions.map((instruction,index)=>(
-                        <div><p>{index+1} {instruction}</p></div>
-                    ))}</p>
-                    <button
-                className="delete button"
-                onClick={() => {
-                  handleDelete(recipe._id);
-                }}
-              >
-                Remove
-              </button>
-                </div>
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      {recipes.map((recipe) => (
+        <div key={recipe.id} className="searchResult">
+          <h2>{recipe.title}</h2>
+          <img src={recipe.image} alt={recipe.title} />
+          <button
+            className="delete button"
+            onClick={() => {
+              handleDelete(recipe._id);
+            }}
+          >
+            Remove
+          </button>
+          <p>{stripHtml(recipe.summary)}</p>
+          <br />
+          <p>
+            Ingredients:
+            {recipe.ingredients.map((ingredient) => (
+              <div>
+                <img
+                  src={`https://spoonacular.com/cdn/ingredients_100x100/${ingredient[1]}`}
+                  alt={ingredient[0]}
+                />
+                <p>{ingredient[0]}</p>
+              </div>
             ))}
+          </p>
+          <br />
+          <p>
+            Instructions:
+            {recipe.instructions.map((instruction, index) => (
+              <div>
+                <p>
+                  {index + 1} {instruction}
+                </p>
+              </div>
+            ))}
+          </p>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
