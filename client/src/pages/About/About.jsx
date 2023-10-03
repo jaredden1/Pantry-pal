05ff60914aa059c6ce633f3330a674568746edb2
jaredden <james.ifit@gmail.com>
2023-10-03 14:49:44 -0400

    styled about page

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -5,21 +5,25 @@ export default function About() {
     <div className="about">
       <h2 className="about-header">Welcome To Pantry Pal</h2>
 
-      <p>Discover all the potential dish options in your pantry with Pantry Pal, your ultimate recipe finder! Ever found yourself wondering what to cook with the random assortment of ingredients you have on hand? Pantry Pal is here to transform those ingredients into mouthwatering dishes.
-      </p>
-
-      <h3 className="about-subheader">How To Use Pantry Pal:</h3>
-      
       <p>
-       Simply enter the ingredients you have on hand in the searchbar, and Pantry Pal will present you with a curated list of delicious recipes tailored to your pantry's content.
-      <br/>
-       Fall in love with a dish? Save the recipe with just a click, so you can revisit and cook it anytime you crave it.   
+        Discover all the potential dish options in your pantry with Pantry Pal,
+        your ultimate recipe finder! Ever found yourself wondering what to cook
+        with the random assortment of ingredients you have on hand? Pantry Pal
+        is here to transform those ingredients into mouthwatering dishes.
       </p>
+      <br />
+      <h3 className="about-subheader">How To Use Pantry Pal:</h3>
 
+      <p>
+        Simply enter the ingredients you have on hand in the searchbar, and
+        Pantry Pal will present you with a curated list of delicious recipes
+        tailored to your pantry's content.
+        <br />
+        Fall in love with a dish? Save the recipe with just a click, so you can
+        revisit and cook it anytime you crave it.
+      </p>
+      <br />
       <h3 className="about-subheader">Technological Stack</h3>
-
-      <p>Technologies used, include:</p>
-      
       <ol className="tech-stack">
         <div>Spoonacular API</div>
         <div>Mongo DB</div>
@@ -27,11 +31,21 @@ export default function About() {
         <div>Node.js</div>
         <div>React</div>
       </ol>
-
+      <br />
       <ul className="developer">
-        <a href="https://www.linkedin.com/in/jamesredden1" className="linkin">James Redden | LinkedIn</a><a href="https://github.com/jaredden1/pantry-pal" className="github-link"> | GitHub Repo</a>
+        James Redden<> | </>
+        <a href="https://www.linkedin.com/in/jamesredden1" className="linkedin">
+          LinkedIn
+        </a>
+        <> | </>
+        <a
+          href="https://github.com/jaredden1/pantry-pal"
+          className="github-link"
+        >
+          GitHub
+        </a>
       </ul>
-      <br/>
+      <br />
       <footer>&copy; 2023 Pantry Pal</footer>
     </div>
   );
